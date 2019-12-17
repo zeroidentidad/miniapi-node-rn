fcafdeff2e7e6827910b2772de4fd028c1509f07
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-12-16 22:01:03 -0600

    rn app
    AsyncStorage simple redux-persist6

--- todoapp/src/redux/store/index.js
+++ todoapp/src/redux/store/index.js
@@ -1,11 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { persistStore, persistReducer} from 'redux-persist';
 import reducer from '../reducer';
+import { AsyncStorage } from 'react-native';
 
 const defaultState = {
     todos: []
 }
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 exports.configureStore = (initialState = defaultState) => {
-    let store = createStore(reducer, initialState)
+    const store = createStore(persistedReducer, initialState)
+    persistStore(store)
     return store;
 }
